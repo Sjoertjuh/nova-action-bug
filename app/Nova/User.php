5aa65223c94ab398c6c2fd086bb7c18d718732d5
Sjoertjuh <63722509+Sjoertjuh@users.noreply.github.com>
2023-11-07 16:08:08 +0100

    remove unneeded authorizes

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -79,33 +79,9 @@ class User extends Resource
     }
 
     /**
-     * Determine if the current user can create new resources.
-     */
-    public static function authorizedToCreate(Request $request): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine if the current user can update the given resource.
-     */
-    public function authorizedToUpdate(Request $request): bool
-    {
-        return false;
-    }
-
-    /**
      * Determine if the current user can delete the given resource.
      */
     public function authorizedToDelete(Request $request): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine if the current user can replicate the given resource.
-     */
-    public function authorizedToReplicate(Request $request): bool
     {
         return false;
     }
